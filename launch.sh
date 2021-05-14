224e28b779749848fc9e65c23b45de039c262ccb
Héctor Romero López <hromerolopez@protonmail.com>
2021-05-14 21:28:31 +0200

    Added parameters and created functions to query cluster status

--- launch.sh
+++ launch.sh
@@ -1,39 +1,108 @@
 #!/bin/bash
 
+vagrant="/opt/vagrant"
+options='delete - Delete the vagrant cluster   create - Create Vagrant cluster \n create-registry - Create Registry VM'
 launch (){
     vagrant up
 }
 
 create_nodes (){
-    for Nnode in {1..2} ; do 
-        vagrant up node-$Nnode
+    n_nodes=$(cat Vagrantfile | grep 'N.=' | awk -F ' ' '{print $NF}')
+    for ((node=1; node<=3;node++)); do
+	$vagrant up node-$node
     done
 }
 
 create_master (){
-    vagrant up eks-master
+    $vagrant up kubernetes-master
 }
 
 create_cluster(){
-    create_nodes
     create_master
+    create_nodes
+}
+
+create_registry(){
+    $vagrant up kubernetes-registry
 }
 
 destroy_cluster(){
-    vagrant destroy
+    $vagrant destroy
 }
 
 register_node(){
-    node_join=$(/opt/vagrant ssh --command "sudo microk8s.add-node | grep 192.168.50" $1)
-    echo $node_join
-    $(/opt/vagrant ssh --command "sudo $node_join" kubernetes-master)
+    # Need to catch up the kubernetes master token to add into nodes
+    node_join=$(/opt/vagrant ssh --command "sudo microk8s.add-node | grep 192.168.50" kubernetes-master)
+    echo "Adding node $1 to kubernetes cluster"
+    echo "Using $node_join"
+    /opt/vagrant ssh --command="sudo $node_join" $1
 }
 
-update_join(){
-    node_join=$(/opt/vagrant ssh --command "sudo microk8s.add-node | grep 192.168.50" $1)
-    echo $node_join >>join.txt
+get_nodes(){
+  command=$(/opt/vagrant ssh --command="sudo microk8s.kubectl get nodes --show-labels" kubernetes-master)
+  echo "[!] List of kubernetes nodes"
+  echo "$command"
+}
+
+cluster_status(){
+
+  for node in node-1 node-2 node-3 kubernetes-master; do
+    status=$(/opt/vagrant ssh --command="sudo microk8s.status" $node | grep 'microk8s is*') >/dev/null
+    echo "[?] $node -- $status"
+  done
+}
+
+add_label_to_node(){
+  # $1 must be the node and $2 the label to add
+  # TODO: Check if node is in cluster and label is in valid format
+  echo "Going to add $2 to $1"
+  result=$(/opt/vagrant ssh --command="sudo microk8s.kubectl label nodes $1 $2" kubernetes-master )
+  echo "Result fom add $2 to $1"
+  echo "$result"
+
+}
+
+delete_vm_node(){
+  echo "[!]Going to delete $1"
+  result=$(/opt/vagrant destroy --force $1)
+  echo "$result"
+}
+
+create_vm_node(){
+  echo "[!] Going to create $1"
+  result=$(/opt/vagrant up $1)
+  echo "$result"
 }
 
 
-update_join node-1
-update_join node-2
+case $1 in
+  delete)
+    destroy_cluster
+    ;;
+  create)
+    create_cluster
+    ;;
+  create-registry)
+    create_nodes
+    ;;
+  nodes-register)
+    register_node $2
+    ;;
+  nodes)
+    get_nodes
+    ;;
+  status)
+    cluster_status
+    ;;
+  nodes-add-label)
+    add_label_to_node $2 $3
+    ;;
+  nodes-vm-delete)
+    delete_vm_node $2
+    ;;
+  nodes-vm-create)
+    create_vm_node $2
+    ;;
+  *)
+    echo "$options"
+esac
