7847e4ac1873b69e7f77af4ab0dbe0c9c47a23ef
Héctor Romero <hector@hromerol.es>
2023-07-13 20:28:10 +0000

    Deleted private registry

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vagrant="/opt/vagrant"
+vagrant="/usr/bin/vagrant"
 options='delete - Delete the vagrant cluster   create - Create Vagrant cluster \n create-registry - Create Registry VM'
 launch (){
     vagrant up
@@ -32,14 +32,14 @@ destroy_cluster(){
 
 register_node(){
     # Need to catch up the kubernetes master token to add into nodes
-    node_join=$(/opt/vagrant ssh --command "sudo microk8s.add-node | grep 192.168.50" kubernetes-master)
+    node_join=$(/usr/bin/vagrant ssh --command "sudo microk8s.add-node  | grep 192.168.50" kubernetes-master)
     echo "Adding node $1 to kubernetes cluster"
     echo "Using $node_join"
-    /opt/vagrant ssh --command="sudo $node_join" $1
+    /usr/bin/vagrant ssh --command="sudo $node_join" $1
 }
 
 get_nodes(){
-  command=$(/opt/vagrant ssh --command="sudo microk8s.kubectl get nodes --show-labels" kubernetes-master)
+  command=$(/usr/bin/vagrant ssh --command="sudo microk8s.kubectl get nodes --show-labels" kubernetes-master)
   echo "[!] List of kubernetes nodes"
   echo "$command"
 }
@@ -47,7 +47,7 @@ get_nodes(){
 cluster_status(){
 
   for node in node-1 node-2 node-3 kubernetes-master; do
-    status=$(/opt/vagrant ssh --command="sudo microk8s.status" $node | grep 'microk8s is*') >/dev/null
+    status=$(/usr/bin/vagrant ssh --command="sudo microk8s.status" $node | grep 'microk8s is*') >/dev/null
     echo "[?] $node -- $status"
   done
 }
@@ -56,7 +56,7 @@ add_label_to_node(){
   # $1 must be the node and $2 the label to add
   # TODO: Check if node is in cluster and label is in valid format
   echo "Going to add $2 to $1"
-  result=$(/opt/vagrant ssh --command="sudo microk8s.kubectl label nodes $1 $2" kubernetes-master )
+  result=$(/usr/bin/vagrant ssh --command="sudo microk8s.kubectl label nodes $1 $2" kubernetes-master )
   echo "Result fom add $2 to $1"
   echo "$result"
 
@@ -64,13 +64,13 @@ add_label_to_node(){
 
 delete_vm_node(){
   echo "[!]Going to delete $1"
-  result=$(/opt/vagrant destroy --force $1)
+  result=$(/usr/bin/vagrant destroy --force $1)
   echo "$result"
 }
 
 create_vm_node(){
   echo "[!] Going to create $1"
-  result=$(/opt/vagrant up $1)
+  result=$(/usr/bin/vagrant up $1)
   echo "$result"
 }
 
